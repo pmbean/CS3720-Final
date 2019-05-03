6fac89f5e26741952e5c5e1b232753407643f458
Patrick Bean <pmbean@live.com>
2019-05-03 13:39:40 -0400

    Initial Commit

---
+++ MyWeather/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyWeather
+//
+//  Created by Patrick Bean on 5/3/19.
+//  Copyright © 2019 TheDevBean. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyWeather/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyWeather
+//
+//  Created by Patrick Bean on 5/3/19.
+//  Copyright © 2019 TheDevBean. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
